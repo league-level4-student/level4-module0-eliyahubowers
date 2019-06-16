456840116b232f83835370de53929f1cdd4e95ce
League Student <league@iMac-93.attlocal.net>
2019-06-16 11:27:48 -0700

    crazyDigitalPainting, conwaysGameOflife IP

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,27 +1,60 @@
 package _01_Crazy_Digital_Painting;
 
+import java.awt.Color;
+import java.util.Random;
+
 public class CrazyDigitalPainting {
 	//1. Create two final static integers for the width and height of the display.
-
+	final static int width = 800;
+	final static int height = 800;
 	
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
-	
+	Color[][] colors = new Color[width][height];
 	
 	
 	public CrazyDigitalPainting() {
 		//3. Open the crazy_digital_painting.png file and look at the image.
-		
+		Random r = new Random();
+		int l = 0;
+		int k = 0;
+		int h = 0;
+		int jj = 0;
 		//4. Iterate through the 2D array and initialize each Color object
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
 		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-		
+		for(int i = 0; i < colors.length; i++) {
+			for(int j = 0; j < colors.length; j++) {
+				l = r.nextInt(2);
+				h = r.nextInt(2);
+				if(l == 0) {
+					l = i;
+				}else if(l == 1) {
+					l = j;
+				}if(h == 0) {
+					h = i;
+				}else if(h == 1) {
+					h = j;
+				}
+				colors[i][j] = new Color(h % 50, l % 255, k % 100);
+				jj = j;
+			}
+			k = r.nextInt(2);
+			if(k == 0) {
+				k = i;
+			}else if(k == 1) {
+				k = jj;
+			}
+		}
 		//5. Come up with your own pattern to make a cool crazy image.
 		
 		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
 		//   to show off your picture.
+		ColorArrayDisplayer cad = new ColorArrayDisplayer();
+		cad.displayColorsAsImage(colors);
+		
 	}
 	
 	public static void main(String[] args) {
--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -3,6 +3,7 @@ package _02_Pixel_Art;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -17,7 +18,7 @@ public class GridPanel extends JPanel{
 	private int cols;
 	
 	//1. Create a 2D array of pixels. Do not initialize it yet.
-	
+	Pixel[][] pixels;
 	private Color color;
 	
 	public GridPanel(int w, int h, int r, int c) {
@@ -34,10 +35,14 @@ public class GridPanel extends JPanel{
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
 		
 		//2. Initialize the pixel array using the rows and cols variables.
-		
+		pixels = new Pixel[rows][cols];
 		
 		//3. Iterate through the array and initialize each element to a new pixel.
-		
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				pixels[i][j] = new Pixel(i*pixelWidth,j*pixelHeight);
+			}
+		}
 		
 	}
 	
@@ -48,6 +53,17 @@ public class GridPanel extends JPanel{
 	public void clickPixel(int mouseX, int mouseY) {
 		//5. Use the mouseX and mouseY variables to change the color
 		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		Random r = new Random();
+		Color c = new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255));
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				if(mouseX > i*pixelWidth && mouseX < (i+1)*pixelWidth) {
+					if(mouseY > j*pixelHeight && mouseY < (j+1)*pixelHeight) {
+						pixels[i][j].color = color;
+					}
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -55,6 +71,13 @@ public class GridPanel extends JPanel{
 		//4. Iterate through the array.
 		//   For every pixel in the list, fill in a rectangle using the pixel's color.
 		//   Then, use drawRect to add a grid pattern to your display.
-		
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				g.setColor(pixels[i][j].color);
+				g.fillRect(i*pixelWidth,j*pixelHeight,pixelWidth,pixelHeight);
+				g.setColor(Color.BLACK);
+				g.drawRect(i*pixelWidth,j*pixelHeight,pixelWidth,pixelHeight);
+			}
+		}
 	}
 }
